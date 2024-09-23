829142e6448ec62c1f9c4f820ad729dc272f420d
Sebastian Llambias <82941241+Sllambias@users.noreply.github.com>
2024-09-23 13:08:22 +0200

    Create GPUJob.sh

---
+++ templates/GPUJob.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -W group_list=P1
+#PBS -A MyName
+#PBS -N TestJob
+#PBS -l nodes=1:ppn=6
+#PBS -l mem=20gb
+#PBS -l walltime=3:00:00
+#PBS -e $PBS_JOBID.err
+#PBS -o $PBS_JOBID.log
+#PBS -j oe
+
+cd $PBS_O_WORKDIR
+
+module load cmake/3.22.2
+module load miniconda3/4.12.0
+conda activate MyEnv
+
+python3 myscript.py
