9465cca6fc8d0296edc2a269ce4c7873ccd87065
Sebastian Llambias <82941241+Sllambias@users.noreply.github.com>
2025-01-16 11:25:50 +0100

    Create ArrayJob.sh

---
+++ templates/ArrayJob.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#PBS -A MyName
+#PBS -N TestJob
+#PBS -l nodes=1:ppn=6:gpus=1
+#PBS -l mem=5gb
+#PBS -l walltime=3:00:00
+#PBS -o $PBS_JOBID.log
+#PBS -j oe
+#PBS -t 1-3 
+
+cd $PBS_O_WORKDIR
+
+module load cmake/3.22.2
+module load miniconda3/4.12.0
+conda activate MyEnv
+
+python3 myscript.py
